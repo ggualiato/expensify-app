95fd715bb54d1a4d3ad09e46251b92349bb24322
ggualiato <ggualiato@gmail.com>
2018-01-17 01:19:13 -0200

    Refactored code using component state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,24 +1,52 @@
-console.log('build-it-visible.js is running')
-
-const appRoot = document.getElementById('app');
-
-let hide = true
-
-const showDetails = () => {
-    hide = !hide
-    renderVisibleApp()
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleShowDetails = this.handleShowDetails.bind(this)
+        this.state = {
+            hide: true
+        }
+    }
+    handleShowDetails() {
+        this.setState((prevState) => {            
+            return {
+                hide: !prevState.hide
+            }
+        })
+    }    
+    render() {
+        return (            
+            <div>
+                <h1>Visibility Toogle</h1>
+                <button onClick={this.handleShowDetails}>{this.state.hide ? 'Show details' : 'Hide details'}</button>
+                <p hidden={this.state.hide}>Details Here!!!</p>
+            </div>
+        )
+    }
 }
 
-const renderVisibleApp = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toogle</h1>
-            <button onClick={showDetails}>{hide ? 'Show details' : 'Hide details'}</button>
-            <p hidden={hide}>Details Here!!!</p>
-        </div>
-    )
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
+
+// console.log('build-it-visible.js is running')
+
+// const appRoot = document.getElementById('app');
+
+// let hide = true
+
+// const showDetails = () => {
+//     hide = !hide
+//     renderVisibleApp()
+// }
+
+// const renderVisibleApp = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toogle</h1>
+//             <button onClick={showDetails}>{hide ? 'Show details' : 'Hide details'}</button>
+//             <p hidden={hide}>Details Here!!!</p>
+//         </div>
+//     )
     
-    ReactDOM.render(template, appRoot)
-}
+//     ReactDOM.render(template, appRoot)
+// }
 
-renderVisibleApp()
+// renderVisibleApp()
