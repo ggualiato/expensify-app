ff1dbeda0789ea61f0a00867e05e22c03f01b60b
ggualiato <ggualiato@gmail.com>
2018-01-20 23:40:50 -0200

    using react lifecycle methods and localstorage

--- src/app.js
+++ src/app.js
@@ -6,8 +6,27 @@ class IndecisionApp extends React.Component {
         this.handleAddOption = this.handleAddOption.bind(this)
         this.handleDeleteOption = this.handleDeleteOption.bind(this)
         this.state = {
-            options: props.options
+            options: []
         }
+    }
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options')
+            const options = JSON.parse(json)
+    
+            if (options) {
+                this.setState(() => ({ options })) 
+            }
+        } catch (error) {}
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options)
+            localStorage.setItem('options', json)
+        }     
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
     }
     handleDeleteOptions() {
         this.setState(() => ({ options: [] }))
@@ -55,10 +74,6 @@ class IndecisionApp extends React.Component {
     }
 }
 
-IndecisionApp.defaultProps = {
-    options: []
-}
-
 const Header = (props) => {
     return (
         <div>
@@ -88,6 +103,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
             {
                 props.options.map((option) => (
                     <Option
@@ -125,12 +141,14 @@ class AddOption extends React.Component {
     handleAddOption(e) {
         e.preventDefault()
 
-        console.log('AddOption');
-
         const option = e.target.elements.option.value.trim()
         const error = this.props.handleAddOption(option)
 
         this.setState(() => ({ error }))
+
+        if (!error) {
+            e.target.elements.option.value = ''
+        }
     }
 
     render() {
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,8 +5,20 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this)
         this.handleReset = this.handleReset.bind(this)
         this.state = {
-            count: props.count
+            count: 0
         }
+    }
+    componentDidMount(){
+        const count = parseInt(localStorage.getItem('count'), 10)
+
+        if (!isNaN(count)) {
+            this.setState(() => ({ count })) 
+        }
+    }
+    componentDidUpdate(prevProps, prevState){
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count)
+        }        
     }
     handleAddOne() {        
         this.setState((prevState) => {
@@ -41,11 +53,7 @@ class Counter extends React.Component {
     }
 }
 
-Counter.defaultProps = {
-    count: 0
-}
-
-ReactDOM.render(<Counter count={10}/>, document.getElementById('app'))
+ReactDOM.render(<Counter count={0}/>, document.getElementById('app'))
 
 // let count = 0
 
