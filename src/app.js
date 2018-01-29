f81b52a85d0d341b759a75623f4118d928d9ebe1
ggualiato <ggualiato@gmail.com>
2018-01-29 00:02:49 -0200

    add client-side routes

--- src/app.js
+++ src/app.js
@@ -1,33 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+
+import AppRouter from './routers/AppRouter'
 
 //styles
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-)
-
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense component
-    </div>
-)
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Route path="/" component={ExpenseDashboardPage} />
-            <Route path="/create" component={AddExpensePage}/>
-        </div>
-
-
-
-    </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById('app'))
+ReactDOM.render(<AppRouter/>, document.getElementById('app'))
