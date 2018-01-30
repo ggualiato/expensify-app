9c39408a6f524067d3e8bff698f8f94abfd16525
Giovanni Gualiato <re035129@resource.com.br>
2018-01-30 16:44:14 -0200

    Setting a CRUD for Redux Store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,15 +2,15 @@ import { createStore } from 'redux'
 
 const incrementCount = ({ incrementBy = 1 } = {}) => ({
     type: 'INCREMENT',
-    incrementBy
+    incrementBy 
 })
 
-const decrementCount = ({ decrementBy = 1 } = {}) => ({
+const decrementCount = ({ decrementBy = 1} = {}) => ({
     type: 'DECREMENT',
     decrementBy
 })
 
-const setCount = (count) => ({
+const setCount = ({ count }) => ({
     type: 'SET',
     count
 })
@@ -19,12 +19,9 @@ const resetCount = () => ({
     type: 'RESET'
 })
 
-// Reducers
-// 1.Reducers are pure functions
-// 2.Never change state or action
-const countReducer = (state = { count: 0 }, action) => {
+const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case 'INCREMENT':            
             return {
                 count: state.count + action.incrementBy
             }
@@ -44,16 +41,14 @@ const countReducer = (state = { count: 0 }, action) => {
         default:
             return state
     }
-}
-
-const store = createStore(countReducer)
+})
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 })
 
 store.dispatch(incrementCount({
-    incrementBy: 5
+    incrementBy: 5 
 }))
 
 store.dispatch(incrementCount())
@@ -64,7 +59,4 @@ store.dispatch(decrementCount())
 
 store.dispatch(decrementCount({ decrementBy: 10 }))
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+store.dispatch(setCount({ count: 101 }))
