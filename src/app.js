2e4692a3cf29f0154e141df6a2ee24ffb07fe7ff
Giovanni Gualiato <re035129@resource.com.br>
2018-01-30 17:59:02 -0200

    Organizing Redux

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import { getVisibleExpenses } from './selectors/expenses'
 
 //styles
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'))
+const store = configureStore()
+
+store.dispatch(addExpense())
+
+console.log(store.getState())
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
