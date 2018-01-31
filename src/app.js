dc3694b784864a804e45e44f0d27c212925e84d0
ggualiato <ggualiato@gmail.com>
2018-01-31 00:39:43 -0200

    First High Order Component

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
-import { getVisibleExpenses } from './selectors/expenses'
+import getVisibleExpenses from './selectors/expenses'
 
 //styles
 import 'normalize.css/normalize.css'
@@ -14,8 +14,14 @@ import './styles/styles.scss'
 
 const store = configureStore()
 
-store.dispatch(addExpense())
+store.dispatch(addExpense( {description: 'Water bill'}))
+store.dispatch(addExpense( {description: 'Gas bill'}))
+store.dispatch(setTextFilter('water'))
 
-console.log(store.getState())
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses);
+
+// console.log(store.getState())
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
