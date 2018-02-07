66db1a6cdeb9c024c87b59aacc8e2a4d656cb1f8
Giovanni Gualiato <re035129@resource.com.br>
2018-02-07 16:30:05 -0200

    add datepicker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import moment from 'moment'
+import 'react-dates/initialize'
+import { SingleDatePicker } from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css'
+
+const now = moment()
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment()
     }
     onDescriptionChange = (e) => {
         const description = e.target.value
@@ -15,10 +22,16 @@ export default class ExpenseForm extends React.Component {
         this.setState(() => ({ note }))
     }
     onAmountChange = (e) => {
-        const amount = e.target.value        
+        const amount = e.target.value
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }))
         }
+    }
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }))
+    }
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }))
     }
     render() {
         return (
@@ -36,6 +49,14 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
                     />
                     <textarea
                         placeholder="Add a note for your expense (optional)"
